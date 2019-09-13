e63d1205d05b8a4b3010046610e187a376b761e5
Grygorii Nakonechnyi <Grygorii.Nakonechnyi@eleks.com>
2019-09-13 13:27:55 +0300

    add remove feature;

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactMapboxGl, { Layer, GeoJSONLayer, Feature, Popup } from "react-mapbox-gl";
+import nearestPoint from '@turf/nearest-point';
+import { point, featureCollection } from '@turf/helpers';
 
 import './App.css';
 
@@ -8,17 +10,32 @@ const currentLocationCoords = [24.000906986937594, 49.80259820083478];
 
 var geojson = {
   type: 'FeatureCollection',
-  features: [{
-    type: 'Feature',
-    geometry: {
-      type: 'Point',
-      coordinates: currentLocationCoords
+  features: [
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: currentLocationCoords
+      },
+      properties: {
+        title: 'Marker1',
+        description: 'marker1 description',
+        score: 0,
+      }
     },
-    properties: {
-      title: 'Marker1',
-      description: 'marker1 description'
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [23.959021610961372, 49.80922878085707]
+      },
+      properties: {
+        title: 'Marker2',
+        description: 'marker2 description',
+        score: 0,
+      }
     }
-  }]
+  ]
 };
 
 class App extends React.Component {
@@ -51,29 +68,39 @@ class App extends React.Component {
     }
   }
 
-  onMarkerHover(marker) {
+  onMarkerHover(map) {
     if (this.state.selectedMarker) {
       return;
     } else{
+      console.info('hovered marker', map.feature)
       this.setState({
-        selectedMarker: marker,
+        selectedMarker: map.feature,
       })
     }
   }
 
-  // onRemoveMarker() {
-  //   const {markers, selectedMarker} = this.state;
+  onRemoveMarker() {
+    const {markers, selectedMarker} = this.state;
+    
+    const selectedPoint = point(selectedMarker.geometry.coordinates);
+    const markersCollections = featureCollection(markers);
+    const nearest = nearestPoint(selectedPoint, markersCollections);
 
-  //   debugger;
-  //   const markersCoords = markers.map(marker => marker.geometry.coordinates);
-  //   const markerToRemoveIndex = markersCoords.findIndex(coords => coords === selectedMarker.lngLat.toArray())
+    const markersCoords = markers.map(marker => marker.geometry.coordinates);
+    const markerToRemoveIndex = markersCoords.findIndex(coords => {
+      return coords[0] === nearest.geometry.coordinates[0] &&
+        coords[1] === nearest.geometry.coordinates[1]
+    });
 
-  //   if (markerToRemoveIndex >= 0) {
-  //     this.setState(prevState => ({
-  //       markers: [...prevState.markers].splice(markerToRemoveIndex, 1)
-  //     }))
-  //   }
-  // }
+    if (markerToRemoveIndex >= 0) {
+      const newMarkers = [...this.state.markers];
+      newMarkers.splice(markerToRemoveIndex, 1);
+      this.setState({
+        markers: newMarkers
+      })
+    }
+    this.onClosePopup();
+  }
 
   onClosePopup() {
     this.setState({
@@ -110,7 +137,14 @@ class App extends React.Component {
         }}
         center={this.state.currentLocation}
         onClick={(map, e) => this.onAddMarker(e.lngLat)}>
-          {/* <GeoJSONLayer data={geojson}/> */}
+          {/* <GeoJSONLayer 
+            data={geojson}
+            symbolLayout={{
+              "text-field": "{place}",
+              "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+              "text-offset": [0, 0.6],
+              "text-anchor": "top"
+            }}/> */}
           <Layer
             type="symbol"
             layout={{ "icon-image": "castle-15"}}
@@ -118,13 +152,15 @@ class App extends React.Component {
               {this.renderMarkers()}
           </Layer>
           {this.state.selectedMarker && <Popup
-            coordinates={this.state.selectedMarker.lngLat.toArray()}
+            coordinates={this.state.selectedMarker.geometry.coordinates}
             offset={{
               'bottom': [0, -10],
             }}
             >
-            <h4>Popup</h4>
+            <h4>Marker</h4>
+            <p>score: 0</p>
             <button onClick={() => this.onClosePopup()}>Close</button>
+            <button onClick={() => this.onRemoveMarker()}>Remove</button>
           </Popup>}
       </Map>
     )
