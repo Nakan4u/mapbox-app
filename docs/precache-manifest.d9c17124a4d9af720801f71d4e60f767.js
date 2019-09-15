2eb69121ec7d46c1d2c76a9b946296f9257bd8fd
Grygorii Nakonechnyi <Grygorii.Nakonechnyi@eleks.com>
2019-09-15 15:44:34 +0300

    add build files as docs folder

---
+++ docs/precache-manifest.d9c17124a4d9af720801f71d4e60f767.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78f916d1811188b380f3825c791637f2",
+    "url": "/mapbox-app/index.html"
+  },
+  {
+    "revision": "82e47b6b093213322658",
+    "url": "/mapbox-app/static/css/2.e71d8db5.chunk.css"
+  },
+  {
+    "revision": "0fc6f7eec9e35122be1d",
+    "url": "/mapbox-app/static/css/main.ce1a2617.chunk.css"
+  },
+  {
+    "revision": "82e47b6b093213322658",
+    "url": "/mapbox-app/static/js/2.f7abab57.chunk.js"
+  },
+  {
+    "revision": "0fc6f7eec9e35122be1d",
+    "url": "/mapbox-app/static/js/main.43e0633f.chunk.js"
+  },
+  {
+    "revision": "d47f1cdd9b869ff2a508",
+    "url": "/mapbox-app/static/js/runtime~main.00be4505.js"
+  }
+]);
